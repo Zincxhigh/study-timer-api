f45b280db44145a4c06fd81178f1c8186747601a
Nooruddin siddiqi <107770982+Zincxhigh@users.noreply.github.com>
2026-03-08 20:31:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -65,4 +65,9 @@ def get_sessions():
 
     return sessions
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 
